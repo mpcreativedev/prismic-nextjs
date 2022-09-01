caef9fc51fb2a703b6b1d2f90f0f60bd0aa8a140
Mauro Piccin <mauro.pccn@gmail.com>
2022-09-01 18:07:35 +0200

    application setup

---
+++ .slicemachine/assets/slices/Title/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Title';
+
+export default {
+  title: 'slices/Title'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Station","spans":[]}],"description":[{"type":"paragraph","text":"Ipsum consectetur et elit aute enim cillum laborum reprehenderit commodo cillum.","spans":[]}]},"slice_type":"title","id":"_Default"}} />
+_Default.storyName = ''
